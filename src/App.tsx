852e34406d934c2f3244622b04f197bca36bdafd
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-08-04 17:24:36 +0400

    feat: implement and check react-router and redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import {Routes, Route} from "react-router-dom";
+import {HomePage} from "./pages/HomePage";
+import {Cart} from "./pages/Cart";
+import React from "react";
 
 function App() {
-  return <div> hello test </div>;
+  return (
+      <Routes>
+          <Route path='/' element={ <HomePage/> }/>
+          <Route path='/cart' element={ <Cart/> }/>
+      </Routes>
+  );
 }
 
 export default App;
