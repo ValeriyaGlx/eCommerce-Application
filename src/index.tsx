10f1f1a3dd7bcacce0daa9d8ab09492e19a4de5c
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-08-09 19:21:44 +0400

    fix: add auto  white space rule

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/App';
 
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <BrowserRouter>
-    <App/>
+    <App />
   </BrowserRouter>,
 );
