5017735b0ba35e98f7d8c82fa050b2130e78aba4
Alexandra376 <suvorovaa1AA@yandex.ru>
2023-08-11 03:32:33 +0300

    feat: add 404 page design

---
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import robot from '../../assets/icons/robot.png';
+import './PageNotFound.scss';
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <div className='pagenotfound'>
+        <div className={'pagenotfound-text'}>
+          <h1>404 page</h1>
+          <h3>Ops... Page not found</h3>
+          <form>
+            <button className={'pagenotfound__button'}>Go Back</button>
+          </form>
+        </div>
+        <div className={'pagenotfound__picture'}>
+          <img
+            className={'pagenotfound___picture__picture'}
+            style={{ width: '100%', height: '61%' }}
+            src={robot}
+            alt='robot'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
