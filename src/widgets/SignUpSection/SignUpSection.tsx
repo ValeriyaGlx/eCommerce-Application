7417ac3691cd0774fb5f8f62326591a7388a8758
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-08-12 17:06:32 +0400

    feat: implenent unmount component with animation

--- src/widgets/SignUpSection/SignUpSection.tsx
+++ src/widgets/SignUpSection/SignUpSection.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import './_SignUpAnimation.scss';
 
 import InputValidation from '../../entities/InputValidation/InputValidation';
 import {
@@ -12,72 +14,84 @@ import InputCheckbox from '../../entities/InputCheckbox/InputCheckbox';
 import Logo from '../../shared/Logo/Logo';
 import { openDropdown } from '../../features/formCommon/openDropdown';
 import { showPassword } from '../../features/formCommon/showPassword';
-import { addDefaulAdress } from '../../features/formCommon/addDefaulAdress';
 
 const SignUpSection = () => {
+  const [checkbox, setCheckbox] = useState(false);
+
+  const checkboxOnChange = () => {
+    setCheckbox(!checkbox);
+  };
   return (
     <section className={'section-signUp'}>
       <div className={'container-logo'}>
         <Logo className={'logo-title-black'} />
       </div>
       <h2 className={'section-signUp_inner'}>Create Account</h2>
-      <div>
-        {signUpArray.map(({ id, type, placeholder, logo, name }) => (
-          <InputValidation
-            key={id}
-            type={type}
-            placeholder={placeholder}
-            logo={logo}
-            showPassword={showPassword}
-            inputName={name}
+      <form>
+        <div>
+          {signUpArray.map(({ id, type, placeholder, logo, name }) => (
+            <InputValidation
+              key={id}
+              type={type}
+              placeholder={placeholder}
+              logo={logo}
+              showPassword={showPassword}
+              inputName={name}
+            />
+          ))}
+        </div>
+        <h4>Address Information</h4>
+        <div>
+          <h5 className={'address-inner'}>Shipping Address</h5>
+          <SelectTag
+            selectArray={selectArray}
+            defaultData={'Choose a country'}
+            className={'singUp-select'}
+            openDropDown={openDropdown}
           />
-        ))}
-      </div>
-      <h4>Address Information</h4>
-      <div>
-        <h5 className={'address-inner'}>Shipping Address</h5>
-        <SelectTag
-          selectArray={selectArray}
-          defaultData={'Choose a country'}
-          className={'singUp-select'}
-          openDropDown={openDropdown}
-        />
-        {addressArray.map(({ type, placeholder, id, name }) => (
-          <InputValidation
-            key={id}
-            type={type}
-            placeholder={placeholder}
-            inputName={'shipping_' + name}
+          {addressArray.map(({ type, placeholder, id, name }) => (
+            <InputValidation
+              key={id}
+              type={type}
+              placeholder={placeholder}
+              inputName={'shipping_' + name}
+            />
+          ))}
+          <InputCheckbox
+            id={'default-address'}
+            data={'Make this address default'}
+            className={'default-address'}
+            onChange={checkboxOnChange}
           />
-        ))}
-        <InputCheckbox
-          id={'default-address'}
-          data={'Make this address default'}
-          className={'default-address'}
-          onChange={addDefaulAdress}
+        </div>
+        <TransitionGroup>
+          {!checkbox && (
+            <CSSTransition classNames='dropdown' timeout={300}>
+              <div>
+                <h5 className={'address-inner'}>Billing Address</h5>
+                <SelectTag
+                  selectArray={selectArray}
+                  defaultData={'Choose a country'}
+                  className={'singUp-select'}
+                  openDropDown={openDropdown}
+                />
+                {addressArray.map(({ type, placeholder, id, name }) => (
+                  <InputValidation
+                    key={id}
+                    type={type}
+                    placeholder={placeholder}
+                    inputName={'billing_' + name}
+                  />
+                ))}
+              </div>
+            </CSSTransition>
+          )}
+        </TransitionGroup>
+        <InputSubmit
+          className={'button-signUp signup_submit-button'}
+          value={'SIGN UP'}
         />
-      </div>
-      <div className={'billing-address_section'}>
-        <h5 className={'address-inner'}>Billing Address</h5>
-        <SelectTag
-          selectArray={selectArray}
-          defaultData={'Choose a country'}
-          className={'singUp-select'}
-          openDropDown={openDropdown}
-        />
-        {addressArray.map(({ type, placeholder, id, name }) => (
-          <InputValidation
-            key={id}
-            type={type}
-            placeholder={placeholder}
-            inputName={'billing_' + name}
-          />
-        ))}
-      </div>
-      <InputSubmit
-        className={'button-signUp signup_submit-button'}
-        value={'SIGN UP'}
-      />
+      </form>
     </section>
   );
 };
