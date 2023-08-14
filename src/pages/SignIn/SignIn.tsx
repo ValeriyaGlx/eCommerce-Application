2c1d3eb90837277eb4f065d8b73dc57ed5d402cb
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-08-14 22:27:49 +0400

    feat: implement modal window

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SignInSection from '../../widgets/SignInSection/SignInSection';
 import NewHereSection from '../../widgets/NewHereSection/NewHereSection';
+import ModalFailed from '../../features/ModalFailed/ModalFailed';
 
 export function SignIn() {
   return (
