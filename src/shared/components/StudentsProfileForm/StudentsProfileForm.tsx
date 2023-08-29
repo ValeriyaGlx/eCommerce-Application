6037e36a51932bd65e173aa9f7b6b650bf8c9a86
Alexandra376 <suvorovaa1AA@yandex.ru>
2023-08-30 03:06:59 +0400

    feat: add two pictures, scss styles for media query and click on icon-pencil

--- src/shared/components/StudentsProfileForm/StudentsProfileForm.tsx
+++ src/shared/components/StudentsProfileForm/StudentsProfileForm.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import './_StudentsProfileForm.scss';
 // eslint-disable-next-line max-len
 import { INPUTS_PROFILE_DATA as profileLinks } from '../../../constants/studentsProfileFormInput/studentsProfileFormInput';
 import InputValidationSignUp from '../../../entities/InputValidationSignUp/view/InputValidationSignUp';
-
+import submit from '../../../assets/icons/daw.png';
+import close from '../../../assets/icons/close.png';
 interface StudentProfileFormProps {
   title: string;
   imageUrl: string;
@@ -14,11 +15,36 @@ export const StudentProfileForm: React.FC<StudentProfileFormProps> = ({
   title,
   imageUrl,
 }) => {
+  const isActive = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    const active = isActive.current;
+    if (active) {
+      active.classList.toggle('active');
+    }
+  };
+
   return (
-    <div className='profile-form'>
-      <div className='profile-form__head'>
-        <div className='profile-form__title'>{title}</div>
-        <img src={imageUrl} alt={'img'} />
+    <div className={'profile-form'}>
+      <div className={'profile-form__head'}>
+        <div className={'profile-form__title'}>{title}</div>
+        <div className={'profile-form__icon'} ref={isActive}>
+          <div className={'profile-form__button'}>
+            <button type='button' onClick={handleClick}>
+              <img src={submit} alt='' />{' '}
+            </button>
+            <button type='button' onClick={handleClick}>
+              <img src={close} alt='' />{' '}
+            </button>
+          </div>
+          <button
+            type='button'
+            className={'profile-form__button--icon'}
+            onClick={handleClick}
+          >
+            <img src={imageUrl} alt='img' />
+          </button>
+        </div>
       </div>
       <div className={'profile-form__input'}>
         {profileLinks.map(
