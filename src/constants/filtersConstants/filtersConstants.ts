c7246f735ced6318927e8cd93090711247686310
stroich <stroichmwork@gmail.com>
2023-08-30 17:43:15 +0400

    fix: add key for the filtersConstants

--- src/constants/filtersConstants/filtersConstants.ts
+++ src/constants/filtersConstants/filtersConstants.ts
@@ -1,10 +1,10 @@
 export const FILTERS_DIFFICULTY_DATA = [
-  { label: 'Easy', value: 'easy' },
-  { label: 'Medium', value: 'medium' },
-  { label: 'Hard', value: 'hard' },
+  { label: 'Easy', value: 'easy', key: Math.random() },
+  { label: 'Medium', value: 'medium', key: Math.random() },
+  { label: 'Hard', value: 'hard', key: Math.random() },
 ];
 
 export const FILTERS_DURATION_DATA = [
-  { label: 'Less 4 weeks', value: 'less 4 weeks' },
-  { label: 'More 4 weeks', value: 'more 4 weeks' },
+  { label: 'Less 4 weeks', value: 'less 4 weeks', key: Math.random() },
+  { label: 'More 4 weeks', value: 'more 4 weeks', key: Math.random() },
 ];
--- src/shared/components/InputRadio/InputRadio.tsx
+++ src/shared/components/InputRadio/InputRadio.tsx
@@ -4,7 +4,7 @@ import './_InputRadio.scss';
 import tick from '../../../assets/icons/check-icn.svg';
 
 interface RadioInputProps {
-  options: { label: string; value: string }[];
+  options: { label: string; value: string; key: number }[];
   onChange: (selectedValue: string) => void;
   className: string;
 }
@@ -25,7 +25,7 @@ export const InputRadio: React.FC<RadioInputProps> = ({
   return (
     <div className={className}>
       {options.map((option) => (
-        <div className={'wrapper-input_radio'}>
+        <div key={option.key} className={'wrapper-input_radio'}>
           <input
             id={option.value}
             type='radio'
