87918ff234369649e58c39bddda6a9e021044bf7
stroich <stroichmwork@gmail.com>
2023-08-30 17:28:54 +0400

    feat: layout of the block with filters

---
+++ src/constants/filtersConstants/filtersConstants.ts
@@ -0,0 +1,10 @@
+export const FILTERS_DIFFICULTY_DATA = [
+  { label: 'Easy', value: 'easy' },
+  { label: 'Medium', value: 'medium' },
+  { label: 'Hard', value: 'hard' },
+];
+
+export const FILTERS_DURATION_DATA = [
+  { label: 'Less 4 weeks', value: 'less 4 weeks' },
+  { label: 'More 4 weeks', value: 'more 4 weeks' },
+];
---
+++ src/shared/components/InputRadio/InputRadio.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import './_InputRadio.scss';
+import tick from '../../../assets/icons/check-icn.svg';
+
+interface RadioInputProps {
+  options: { label: string; value: string }[];
+  onChange: (selectedValue: string) => void;
+  className: string;
+}
+
+export const InputRadio: React.FC<RadioInputProps> = ({
+  options,
+  onChange,
+  className,
+}) => {
+  const [selectedValue, setSelectedValue] = useState<string>('');
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSelectedValue(value);
+    onChange(value);
+  };
+
+  return (
+    <div className={className}>
+      {options.map((option) => (
+        <div className={'wrapper-input_radio'}>
+          <input
+            id={option.value}
+            type='radio'
+            name='radioInput'
+            value={option.value}
+            checked={selectedValue === option.value}
+            onChange={handleRadioChange}
+          />
+
+          <label htmlFor={option.value} key={option.value}>
+            <span>
+              <img src={tick} alt='Checked Icon' />
+            </span>
+            {option.label}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+};
