a74606b9a4f5eb34f8aa3370e4c18c23e6cb5c20
Alexandra376 <suvorovaa1AA@yandex.ru>
2023-09-01 00:18:46 +0400

    feat: add scss styles for button

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -3,18 +3,10 @@ import './_Profile.scss';
 
 import StudentProfileForm from '../../shared/components/StudentsProfileForm/StudentsProfileForm';
 import StudentsProfileCollage from '../../shared/StudentsProfileCollage/StudentsProfileCollage';
-import ButtonWithRoute from '../../shared/components/ButtonWithRoute/ButtonWithRoute';
 
 export function Profile() {
   return (
     <>
-      <div className={'profile-container__button'}>
-        <ButtonWithRoute
-          className={'button-signIn button-main-products'}
-          path={'/products'}
-          data={'Load More'}
-        />
-      </div>
       <div className='wrapper-profile'>
         <StudentProfileForm />
         <StudentsProfileCollage />
