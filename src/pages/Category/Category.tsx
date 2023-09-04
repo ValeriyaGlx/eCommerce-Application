2791fccc1e52b94df3578b8264a03d788633bdbb
stroich <stroichmwork@gmail.com>
2023-09-04 12:56:27 +0400

    fix: fix the transition to non-working categories and sub-categories

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Breadcrumbs from '../../shared/components/Breadcrumb/Breadcrumbs';
 import ProductWithNavigation from '../../features/ProductsWithNavigation/ProductWithNavigation';
+import PageNotFound from '../PageNotFound/PageNotFound';
+import { CATEGORIES_OF_PRODUCTS as categories } from '../../constants/productsConstant/productsConstants';
 
 const Category = () => {
+  const [pageFound, setPageFound] = useState(true);
   const { categoryId } = useParams();
+
+  useEffect(() => {
+    const categoryObj = categories.find((item) => item.data === categoryId);
+    if (categoryObj === undefined) setPageFound(false);
+  }, []);
+
   if (categoryId) {
     const breadcrumb = [
       {
@@ -20,18 +29,23 @@ const Category = () => {
       },
     ];
     return (
-      <main>
-        <div className={'container products-inner'}>
-          <h1>
-            Discover Our Range of
-            <span className={'products-inner-colorful'}>
-              Products & Categories
-            </span>
-          </h1>
-          <Breadcrumbs breadcrumbs={breadcrumb} />
-          <ProductWithNavigation category={categoryId} />
-        </div>
-      </main>
+      <>
+        {!pageFound && <PageNotFound />}
+        {pageFound && (
+          <main>
+            <div className={'container products-inner'}>
+              <h1>
+                Discover Our Range of
+                <span className={'products-inner-colorful'}>
+                  Products & Categories
+                </span>
+              </h1>
+              <Breadcrumbs breadcrumbs={breadcrumb} />
+              <ProductWithNavigation category={categoryId} />
+            </div>
+          </main>
+        )}
+      </>
     );
   }
 };
