0c0662aa3afa68698abb2bd6bb67987181fe38b5
stroich <stroichmwork@gmail.com>
2023-09-11 20:24:49 +0400

    feat: add a delete button for the product page

--- src/features/ProductDescription/ProductDescription.tsx
+++ src/features/ProductDescription/ProductDescription.tsx
@@ -4,10 +4,13 @@ import './_ProductDescription.scss';
 import Like from '../../shared/components/Like/Like';
 import {
   addProductToCart,
+  findIdByProductId,
   idOfProductToCart,
+  removeProductFromCart,
 } from '../../entities/ApiCart/addProductToCart';
 import ShoppingCartButton from '../../shared/components/ShoppingCardButton/ShoppingCartButton';
 import getCookie from '../../shared/cookie/getCookie';
+import { getCartById } from '../../entities/ApiCart/ApiCart';
 
 interface ProductDescriptionProps {
   inner: string;
@@ -29,6 +32,7 @@ const ProductDescription: FC<ProductDescriptionProps> = ({
   productId,
 }) => {
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -46,9 +50,29 @@ const ProductDescription: FC<ProductDescriptionProps> = ({
     fetchData();
   }, []);
 
-  async function clickCart(event: React.MouseEvent) {
+  async function addCart(event: React.MouseEvent) {
+    setIsAnimating(true);
     await addProductToCart(event, productId);
     setIsButtonDisabled(true);
+    setTimeout(() => setIsAnimating(false), 300);
+  }
+
+  async function removeFromCart() {
+    setIsAnimating(true);
+    const cart = getCookie('cartId') as string;
+    const anonToken = getCookie('anonToken') as string;
+    const authToken = getCookie('authToken') as string;
+    let obj;
+    if (anonToken) {
+      obj = await getCartById(cart, anonToken);
+    }
+    if (authToken) {
+      obj = await getCartById(cart, authToken);
+    }
+    const idLineItem = findIdByProductId(obj.lineItems, productId) as string;
+    await removeProductFromCart(idLineItem);
+    setIsButtonDisabled(false);
+    setTimeout(() => setIsAnimating(false), 300);
   }
 
   return (
@@ -78,12 +102,21 @@ const ProductDescription: FC<ProductDescriptionProps> = ({
           </div>
           <Like />
         </div>
-        <ShoppingCartButton
-          isDisabled={isButtonDisabled}
-          className={'buy-now-button'}
-          data={'Buy Now'}
-          onClick={(event) => clickCart(event)}
-        />
+        {!isButtonDisabled ? (
+          <ShoppingCartButton
+            isDisabled={isButtonDisabled}
+            className={`buy-now-button ${isAnimating ? 'change' : ''}`}
+            data={'Buy Now'}
+            onClick={(event) => addCart(event)}
+          />
+        ) : (
+          <ShoppingCartButton
+            isDisabled={false}
+            className={`remove-button ${isAnimating ? 'change' : ''}`}
+            data={'Remove from Cart'}
+            onClick={removeFromCart}
+          />
+        )}
       </div>
     </div>
   );
