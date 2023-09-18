e1180179412309aa2d671d6a4ca288f2a401c773
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-09-18 19:33:32 +0400

    fix: fix abouts us

--- src/shared/AboutUsTeamPhoto/AboutUsTeamPhoto.tsx
+++ src/shared/AboutUsTeamPhoto/AboutUsTeamPhoto.tsx
@@ -9,11 +9,11 @@ const AboutUsTeamPhoto = () => {
             <span className='color'>We are DigiSet team</span>
           </h1>
           <p className='about-container__story'>
-            <span className='color-orange'>This is a story</span> of effective
-            cooperation, passion for information technology and the desire to
-            help others master this fascinating profession. Cohesive work and
-            mutual assistance were able to ensure the succseful completion of
-            the project.
+            <span className='color-orange'>Our journey is </span> a testament to
+            the power of effective collaboration, a shared passion for
+            information technology, and a drive to empower others in mastering
+            this intriguing field. Our success in completing this project was a
+            result of seamless teamwork and mutual support.
           </p>
         </div>
         <div className={'about-regards'}>
