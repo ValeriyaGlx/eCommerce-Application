d1b8d6bf3a821203acd66e83fd2d9e409d9d3a46
stroich <stroichmwork@gmail.com>
2023-09-18 10:51:26 +0400

    fix: fix the page when switching to a category

--- src/features/CategoryNavigation/CategoryNavigation.tsx
+++ src/features/CategoryNavigation/CategoryNavigation.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 
 import { CATEGORIES_OF_PRODUCTS as categories } from '../../constants/productsConstant/productsConstants';
 import Button from '../../shared/components/Button/Button';
+import { setCurrentPage } from '../../app/store/actions/paginationAction/paginationSlice';
+import { store } from '../../app/store/store';
+
+type RootState = ReturnType<typeof store.getState>;
 
 const CategoryNavigation = () => {
+  const dispatch: ThunkDispatch<RootState, unknown, AnyAction> = useDispatch();
   const navigate = useNavigate();
   const handleCategoryClick = async (data: string) => {
+    dispatch(setCurrentPage(1));
     if (data !== 'All Categories') {
       navigate(`/products/${data}`);
     }
